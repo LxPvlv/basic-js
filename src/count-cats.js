0e2962737d1b42723c4bbe3c7c90398fb35cc08b
lxpvlv <w.lxpvlv@gmail.com>
2020-02-24 20:55:06 +0500

    feat: add solution for count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  return backyard.reduce(
+    (allCats, places) =>
+      places.reduce(
+        (cats, place) => (place === "^^" ? cats + 1 : cats),
+        allCats
+      ),
+    0
+  );
 };
