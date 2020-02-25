71a442d8f9c4ea9a27b1a838021f89ee175ee8af
lxpvlv <w.lxpvlv@gmail.com>
2020-02-25 16:47:05 +0500

    feat: add solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,35 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw Error;
+
+  const result = [];
+  for (let i = 0, j = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--discard-next":
+        i++;
+        break;
+      case "--discard-prev":
+        if(j>0){
+          result.pop()
+          j--
+        }
+        break;
+      case "--double-next":
+        if (i+1 < arr.length) {
+          result[j] = arr[i + 1];
+          j++;
+        }
+        break;
+      case "--double-prev":
+        if(i!==0){
+          result[j] = arr[i - 1];
+          j++;
+        }
+        break;
+      default:
+        result[j] = arr[i];
+        j++;
+    }
+  }
+
+  return result;
 };
