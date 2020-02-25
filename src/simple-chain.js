c9ee5d3adab212386090fcf46f8b70284035bebe
lxpvlv <w.lxpvlv@gmail.com>
2020-02-25 21:24:28 +0500

    feat: add solution for simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,41 @@
-const chainMaker = {
+module.exports = {
+  chain: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (Object.is(value, null)) value = "null";
+    if (Object.is(value, NaN)) value = "NaN";
+
+    this.chain.push(value);
+    return this;
   },
+
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (!Number.isInteger(position)) {
+      this.chain = [];
+      throw Error;
+    }
+
+    if (position < 1 || position > this.chain.length) {
+      this.chain = [];
+      throw Error;
+    }
+
+    this.chain.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    const strChain = `( ${this.chain.join(" )~~( ")} )`;
+    this.chain = [];
+    return strChain;
   }
 };
-
-module.exports = chainMaker;
