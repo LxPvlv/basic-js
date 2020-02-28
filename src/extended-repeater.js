bb033f0668a581348a3700071f21dfe185970b8f
lxpvlv <w.lxpvlv@gmail.com>
2020-02-29 02:28:26 +0500

    feat: add solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,18 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
-  
+module.exports = (
+  str,
+  {
+    repeatTimes = 0,
+    separator = "+",
+    addition = "",
+    additionRepeatTimes = 0,
+    additionSeparator = "|"
+  }
+) =>
+  new Array(repeatTimes || 1)
+    .fill(
+      String(str) +
+        new Array(additionRepeatTimes || 1)
+          .fill(String(addition))
+          .join(additionSeparator)
+    )
+    .join(separator);
