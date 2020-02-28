9ff7ce878e84c91e4d9c52d23f61146fde29dd5a
lxpvlv <w.lxpvlv@gmail.com>
2020-02-28 23:45:39 +0500

    refactor: calculateDepth with one argument

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,10 +1,9 @@
 module.exports = class DepthCalculator {
-  calculateDepth(arr, depth = 1) {
-    return Math.max(
-      depth,
-      ...arr
-        .filter(Array.isArray)
-        .map(item => this.calculateDepth(item, depth + 1))
-    );
+  calculateDepth(arr) {
+    if(!Array.isArray(arr)) return 0;
+    const filtered = arr.filter(Array.isArray)
+    if(filtered.length ===0) return 1
+
+    return Math.max(...filtered.map(a=>this.calculateDepth(a)+1))
   }
 };
